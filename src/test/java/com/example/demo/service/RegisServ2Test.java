a72faf825ff90c88a427bdbda791e7d894bff599
DimaGotaro <gotaros@yandex.by>
2022-12-24 02:25:12 +0300

    fix36 build.yml

--- src/test/java/com/example/demo/service/RegisServ2Test.java
+++ src/test/java/com/example/demo/service/RegisServ2Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,9 +19,11 @@ class RegisServ2Test {
     private RegisServ regisServ;
 
     @Test
+    @Sql(value = {"/create-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/create-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void reg21() {
         User user = new User();
-        user.setUsername("Admin");
+        user.setUsername("Dima");
         Map<String, Object> map = new HashMap<>();
 
         String s = regisServ.regAdd(user, map);
