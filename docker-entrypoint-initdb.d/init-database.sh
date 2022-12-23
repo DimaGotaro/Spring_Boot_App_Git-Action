f8a305115e24d5449749804567f9d3e880f66be9
DimaGotaro <gotaros@yandex.by>
2022-12-24 01:25:36 +0300

    fix31 build.yml

--- docker-entrypoint-initdb.d/init-database.sh
+++ docker-entrypoint-initdb.d/init-database.sh
@@ -2,7 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER postgres;
     CREATE DATABASE kacher;
     GRANT ALL PRIVILEGES ON DATABASE kacher TO postgres;
     CREATE DATABASE public;
